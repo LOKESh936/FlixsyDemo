c6d63709c5d739d7d24bcf8024004ff6cf0bdae4
Loki <lokeshwar936@gmail.com>
2026-05-03 14:01:16 -0500

    Initial Commit

---
+++ FlixsyDemo/FlixsyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixsyDemoApp.swift
+//  FlixsyDemo
+//
+//  Created by Lokeshwar Reddy Malli reddy on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FlixsyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
